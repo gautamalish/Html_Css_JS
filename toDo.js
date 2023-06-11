d0d99c4b7c22fa469b739fbc4bfca74a2c58f6fd
JustSuman <np03cs4a220062@heraldcollege.edu.np>
2023-06-11 22:31:39 +0545

    added few changes

--- toDo.js
+++ toDo.js
@@ -1,57 +1,52 @@
-let input=document.getElementById("input");
-let tasks=document.getElementById("tasks");
-let submit=document.getElementById("submit");
-let time=document.getElementById("time")
+let input = document.getElementById("input");
+let tasks = document.getElementById("tasks");
+let submit = document.getElementById("submit");
+let time = document.getElementById("time");
 
-
-
-function search(e){
-    e.preventDefault();
-    let inputVal=input.value
-    create(inputVal)
+function search(e) {
+  e.preventDefault();
+  let inputVal = input.value;
+  input.value = "";
+  create(inputVal);
 }
 
-function create(inputVal){
-    div=document.createElement("div")
-    div.setAttribute("class","task")
-    
-    const innerDiv=document.createElement("div");
-    innerDiv.textContent=inputVal;
+function create(inputVal) {
+  div = document.createElement("div");
+  div.setAttribute("class", "task");
 
-    const button=document.createElement("button")
-    button.innerHTML="-"
-    button.setAttribute("class","button")
-    div.appendChild(innerDiv);
-    div.appendChild(button);
-    tasks.appendChild(div)
+  const li = document.createElement("li");
+  li.textContent = inputVal;
 
-    button.addEventListener("click",()=>{
-        const parent=button.parentNode;
-        tasks.removeChild(parent)
-})
+  const button = document.createElement("button");
+  button.innerHTML = "-";
+  button.setAttribute("class", "button");
+  div.appendChild(li);
+  div.appendChild(button);
+  tasks.appendChild(div);
+
+  button.addEventListener("click", () => {
+    const parent = button.parentNode;
+    tasks.removeChild(parent);
+  });
 }
 
-submit.addEventListener("click",search)
+submit.addEventListener("click", search);
 
-function getDate(){
-
-    let date=new Date;
-    let hours=date.getHours();
-    let minutes=date.getMinutes();
-    let secs=date.getSeconds();
-    let ampm=hours>12?"PM":"AM"
-    hours=(hours%12)||12
-    hours=AddingZeros(hours)
-    minutes=AddingZeros(minutes)
-    secs=AddingZeros(secs)
-    let result=`${hours}:${minutes}:${secs}${ampm}`
-    time.innerHTML=result
+function getDate() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let secs = date.getSeconds();
+  let ampm = hours > 12 ? " PM" : " AM";
+  hours = hours % 12 || 12;
+  hours = AddingZeros(hours);
+  minutes = AddingZeros(minutes);
+  secs = AddingZeros(secs);
+  let result = `${hours}:${minutes}:${secs}${ampm}`;
+  time.innerHTML = result;
 }
-function AddingZeros(time){
-    time=time.toString();
-    return time.length<2?"0"+time:time
+function AddingZeros(time) {
+  time = time.toString();
+  return time.length < 2 ? "0" + time : time;
 }
-setInterval(getDate,1000)
-
-
-
+setInterval(getDate, 1000);
